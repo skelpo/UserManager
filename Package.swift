1c443cc4fe669debd3ee8f9d344812471c787fb0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-30 12:07:56 -0500

    Removed commented-out dependency. Changed JWTVapor git URL from SSH to HTTPS form

--- Package.swift
+++ Package.swift
@@ -12,12 +12,11 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
-//        .package(url: "https://github.com/skelpo/aws.git", .upToNextMajor(from: "1.0.1")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.9.0"),
         .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.0"),
         .package(url: "https://github.com/skelpo/lingo-provider.git", from: "2.0.0-rc.1.1"),
         .package(url: "https://github.com/skelpo/JWTDataProvider.git", from: "0.10.0-rc.2"),
-        .package(url: "git@github.com:skelpo/JWTVapor.git", from: "0.5.0"),
+        .package(url: "https://github.com/skelpo/JWTVapor.git", from: "0.5.0"),
         .package(url: "https://github.com/skelpo/SkelpoMiddleware.git", from: "1.4.0-rc.3.1")
     ],
     targets: [
