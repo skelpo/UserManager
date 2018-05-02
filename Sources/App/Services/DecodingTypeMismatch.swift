8c831ea8cbf6411007b6f7fab8b549fc11d0f054
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-02 11:58:03 -0500

    Added quotes around expected type for DecodingTypeMismatch result message

--- Sources/App/Services/DecodingTypeMismatch.swift
+++ Sources/App/Services/DecodingTypeMismatch.swift
@@ -6,7 +6,7 @@ struct DecodingTypeMismatch: ErrorCatchingSpecialization {
     func convert(error: Error, on request: Request) -> ErrorResult? {
         if case let DecodingError.typeMismatch(type, context) = error {
             let path = context.codingPath.map { $0.stringValue }.joined(separator: ".")
-            return ErrorResult(message: "Decoding request body failed. Make sure the '\(path)' key exists and is of type \(type)", status: .badRequest)
+            return ErrorResult(message: "Decoding request body failed. Make sure the '\(path)' key exists and is of type '\(type)'", status: .badRequest)
         }
         return nil
     }
