123afd6a8020b67453d83c0b769e5362777c0180
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-02 07:32:50 -0500

    Added expected type to DecodingTypeMismatch error message

--- Sources/App/Services/DecodingTypeMismatch.swift
+++ Sources/App/Services/DecodingTypeMismatch.swift
@@ -4,9 +4,9 @@ import Vapor
 /// Converts `DecodingError.typeMismatch` errors to a human friendly response.
 struct DecodingTypeMismatch: ErrorCatchingSpecialization {
     func convert(error: Error, on request: Request) -> ErrorResult? {
-        if case let DecodingError.typeMismatch(_, context) = error {
+        if case let DecodingError.typeMismatch(type, context) = error {
             let path = context.codingPath.map { $0.stringValue }.joined(separator: ".")
-            return ErrorResult(message: "Decoding request body failed. Make sure the '\(path)' key exists and is the correct type", status: .badRequest)
+            return ErrorResult(message: "Decoding request body failed. Make sure the '\(path)' key exists and is of type \(type)", status: .badRequest)
         }
         return nil
     }
