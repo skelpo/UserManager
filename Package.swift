699f87021779f96e5197f97ab5aa99af033b65ea
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-08 07:31:29 -0500

    Updated Vapor to official 3.0 release

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
         .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.9.0"),
-        .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.0"),
-        .package(url: "https://github.com/skelpo/lingo-provider.git", from: "2.0.0-rc.1.1"),
-        .package(url: "https://github.com/skelpo/JWTDataProvider.git", from: "0.10.0-rc.2"),
-        .package(url: "https://github.com/skelpo/JWTVapor.git", from: "0.5.0"),
-        .package(url: "https://github.com/skelpo/SkelpoMiddleware.git", from: "1.4.0-rc.3.1")
+        .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.3"),
+        .package(url: "https://github.com/skelpo/lingo-provider.git", from: "3.0.0"),
+        .package(url: "https://github.com/skelpo/JWTDataProvider.git", from: "0.10.0"),
+        .package(url: "https://github.com/skelpo/JWTVapor.git", from: "0.9.0"),
+        .package(url: "https://github.com/skelpo/SkelpoMiddleware.git", from: "1.4.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "FluentMySQL", "JWT", "CryptoSwift", "SendGrid", "LingoVapor", "JWTDataProvider", "JWTVapor", "SkelpoMiddleware"],
